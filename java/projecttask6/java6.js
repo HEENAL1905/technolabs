8282eaaa1e25c0a79854a867dc36b8a12fc80f0f
HEENAL1905 <70204831+HEENAL1905@users.noreply.github.com>
2021-04-28 11:40:31 +0530

    Update java6.js

--- java/projecttask6/java6.js
+++ java/projecttask6/java6.js
@@ -1,7 +1,7 @@
 function temp(){
-const kelvin=293;
-const celcius=kelvin-273;
-var fahrenheit= celcius*(9/5)+32;
-var fahrenheit=Math.floor(fahrenheit);
+const kelvin=293; // defining constant variable kelvin 
+const celcius=kelvin-273; // converting kelvin to celcius
+var fahrenheit= celcius*(9/5)+32; // converting celcius to fahrenheit 
+var fahrenheit=Math.floor(fahrenheit); // using math library function floor to remove decimal places 
 console.log("The temperature is "+ fahrenheit +" degrees Fahrenheit");
 }
