55a423a40d8e37446615c8c6428567b89306380b
HEENAL1905 <70204831+HEENAL1905@users.noreply.github.com>
2021-04-28 11:27:56 +0530

    Create java6.js

---
+++ java/projecttask6/java6.js
@@ -0,0 +1,7 @@
+function temp(){
+const kelvin=293;
+const celcius=kelvin-273;
+var fahrenheit= celcius*(9/5)+32;
+var fahrenheit=Math.floor(fahrenheit);
+console.log("The temperature is "+ fahrenheit +" degrees Fahrenheit");
+}
