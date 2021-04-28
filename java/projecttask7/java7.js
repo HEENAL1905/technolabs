e0654ed63942971ebb1befa96bb0038aa5e8ba69
HEENAL1905 <70204831+HEENAL1905@users.noreply.github.com>
2021-04-28 11:39:05 +0530

    Create java7.js

---
+++ java/projecttask7/java7.js
@@ -0,0 +1,12 @@
+function Dogyears(){
+const myAge=20;
+var earlyYears=2;
+ earlyYears=10.5*earlyYears
+var laterYears=myAge-2;
+laterYears=laterYears*4;
+const myAgeInDogYears=laterYears+earlyYears // calculating my age in dogyears 
+var myName="Heenal";
+myName=myName.toLowerCase();
+console.log("My name is "+myName+". I am "+myAge+" years old in human years which is "
++myAgeInDogYears+" years old in dog years.");
+}
